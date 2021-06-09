47a7263ca66210d58b16520a172c4289b8e1319f
Moova Shaka <mrmoovashaka@gmail.com>
2021-06-09 12:20:01 +0100

    adding images

--- src/layout/Carousel.js
+++ src/layout/Carousel.js
@@ -2,30 +2,64 @@ import './css/carousel.css'
 import logo from '../images/logo.svg';
 import logo1 from '../images/logo1.svg';
 import logo2 from '../images/logo2.svg';
+import logo3 from '../images/swllogo5.svg';
+import logoblk from '../images/swllogoblk.svg';
+import homeoffice from '../images/homeoffice795.svg';
+import homeofficelogo from '../images/homeofficelogo.svg';
 
 function Carousel() {
   return (
-<div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img src={logo} className="d-block w-100" alt="logo" />
+     <div id="myCarousel" className="carousel slide" data-bs-ride="carousel">
+     <hr className="featurette-divider" />
+      <div className="carousel-indicators">
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src={homeofficelogo} width="60%" height="60%"/>
+
+          <div className="container">
+            <div className="carousel-caption text-start">
+              <h1>Beautiful Garden Structures.</h1>
+              <p>Bring your garden to life with style.</p>
+              <p><a className="btn btn-lg btn-primary" href="#">Sign up today</a></p>
+            </div>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src={homeoffice} width="60%" height="60%"/>
+          <div className="container">
+            <div className="carousel-caption">
+              <h1>Another example headline.</h1>
+              <p>Some representative placeholder content for the second slide of the carousel.</p>
+              <p><a className="btn btn-lg btn-primary" href="#">Learn more</a></p>
+            </div>
+          </div>
+        </div>
+        <div className="carousel-item">
+          <img src={homeoffice} width="60%" height="80%"/>
+
+          <div className="container">
+            <div className="carousel-caption text-end">
+              <h1>SWL Beautiful Creations</h1>
+              <p>Some representative placeholder content for the third slide of this carousel.</p>
+              <p><a className="btn btn-lg btn-primary" href="#">Browse gallery</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button className="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+      <hr />
     </div>
-    <div className="carousel-item">
-      <img src={logo1}  alt="logo1" className="d-block w-100" />
-    </div>
-    <div className="carousel-item">
-      <img src={logo2} alt="logo2" className="d-block w-100" />
-    </div>
-  </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
-</div>
 );
 }
 
--- src/layout/Nav.js
+++ src/layout/Nav.js
@@ -1,32 +1,34 @@
 function Nav() {
   return(
 <header>
-  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Carousel</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+  <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div className="container-fluid">
+      <a className="navbar-brand" href="#">Carousel</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav me-auto mb-2 mb-md-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+      <div className="collapse navbar-collapse" id="navbarCollapse">
+        <ul className="navbar-nav me-auto mb-2 mb-md-0">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Home</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#" />Link>
+          <li className="nav-item">
+            <a className="nav-link" href="#" />Link>
           </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true" >Disabled</a>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true" >Disabled</a>
           </li>
         </ul>
-        <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-          <button class="btn btn-outline-success" type="submit">Search</button>
+        <form className="d-flex">
+          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+          <button className="btn btn-outline-success" type="submit">Search</button>
         </form>
       </div>
     </div>
   </nav>
+  <hr />
 </header>
+
 );
 }
 
